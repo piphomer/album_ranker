0267e0dab013212391581b4f2edd7966958e15cb
piphomer <phillippeterhomer@gmail.com>
2023-08-06 20:06:11 +0100

    Added several more iterations of the ranking algorithm. All are contained in this commit.

--- main.py
+++ main.py
@@ -1,95 +1,40 @@
 import sqlite3
 import pandas as pd
 import numpy as np
+import os
+import sys
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-pd.options.display.width = 110
+pd.options.display.width = 250
 
 def read_db():
-    dbfile = 'C:/Users/phill/Music/MM_20230719070329.DB'
-    con = sqlite3.connect(dbfile)
-    cur = con.cursor()
+    dbfile = 'C:/Users/phill/Music/MM_20230726222029.DB'
+    if os.path.isfile(dbfile):
+        con = sqlite3.connect(dbfile)
+        cur = con.cursor()
 
-    sql = """
-        SELECT AlbumArtist, Album, SongTitle, Rating, SongLength
-        FROM Songs
-        WHERE length(Album) > 0
-        """
+        sql = """
+            SELECT AlbumArtist, Album, SongTitle, Rating, SongLength
+            FROM Songs
+            WHERE length(Album) > 0
+            """
 
-    for row in cur.execute(sql):
+        for row in cur.execute(sql):
 
-        output_list.append(row)
+            input_list.append(row)
 
-    con.close
+        con.close
+
+    else:
+        print("Database file not found")
+        sys.exit()
 
 def create_dataframe(input_list):
 
-    album_df = pd.DataFrame(input_list, columns=['Artist', 'Album', 'Title', 'Rating', 'Duration'])
+    df = pd.DataFrame(input_list, columns=['Artist', 'Album', 'Title', 'Rating', 'Duration'])
 
-    return album_df
-
-def create_test_dataframe():
-
-    test_list = [
-        ('Oasis', 'Definitely Maybe', 'Rock & Roll Star', 40, 328217),
-        ('Oasis', 'Definitely Maybe', 'Shakermaker', 40, 313217),
-        ('Oasis', 'Definitely Maybe', 'Live Forever', 90, 281642),
-        ('Oasis', 'Definitely Maybe', 'Up In The Sky', 50, 273668),
-        ('Oasis', 'Definitely Maybe', 'Columbia', 40, 381763),
-        ('Oasis', 'Definitely Maybe', 'Supersonic', 80, 288486),
-        ('Oasis', 'Definitely Maybe', 'Bring It On Down', 40, 262593),
-        ('Oasis', 'Definitely Maybe', 'Cigarettes & Alcohol', 60, 294149),
-        ('Oasis', 'Definitely Maybe', 'Slide Away', 80, 397489),
-        ('Oasis', 'Definitely Maybe', 'Married With Children', 70, 197130),
-        ('The National', 'Alligator', 'Secret Meeting', 80, 233736),
-        ('The National', 'Alligator', 'Abel', 70, 225928),
-        ('The National', 'Alligator', 'The Geese Of Beverly Road', 60, 305672),
-        ('The National', 'Alligator', 'City Middle', 50, 276488),
-        ('The National', 'Alligator', 'Mr. November', 60, 249480),
-        ('The National', 'Alligator', 'Karen', 80, 248072),
-        ('The National', 'Alligator', 'Lit Up', 60, 183944),
-        ('The National', 'Alligator', 'Looking For Astronauts', 40, 212104),
-        ('The National', 'Alligator', 'Daughters Of The Soho Riots', 70, 247560),
-        ('The National', 'Alligator', 'Friend Of Mine', 40, 214152),
-        ('The National', 'Alligator', 'Val Jester', 40, 189576),
-        ('The National', 'Alligator', 'All The Wine', 60, 204296),
-        ('Rickie Lee Jones', 'Rickie Lee Jones', 'On Saturday Afternoons In 1963', 60, 155506),
-        ('Rickie Lee Jones', 'Rickie Lee Jones', 'Night Train', 80, 198896),
-        ('Rickie Lee Jones', 'Rickie Lee Jones', 'Young Blood', 50, 247849),
-        ('Rickie Lee Jones', 'Rickie Lee Jones', 'Easy Money', 60, 200280),
-        ('Rickie Lee Jones', 'Rickie Lee Jones', 'The Last Chance Texaco', 40, 248241),
-        ('Rickie Lee Jones', 'Rickie Lee Jones', 'Coolsville', 50, 232829),
-        ('Rickie Lee Jones', 'Rickie Lee Jones', 'Weasel & The White Boys Cool', 80, 363859),
-        ('Rickie Lee Jones', 'Rickie Lee Jones', 'Company', 40, 294817),
-        ('Rickie Lee Jones', 'Rickie Lee Jones', 'After Hours', 40, 134243),
-        ('Sarah Blasko', 'The Overture & The Underscore', 'All Coming Back', 40, 195976),
-        ('Sarah Blasko', 'The Overture & The Underscore', 'Beautiful Secrets', -1, 204296),
-        ('Sarah Blasko', 'The Overture & The Underscore', 'Always Worth It', 50, 227080),
-        ('Sarah Blasko', 'The Overture & The Underscore', 'At Your Best', 50, 216584),
-        ('Sarah Blasko', 'The Overture & The Underscore', 'Counting Sheep', 50, 261256),
-        ('Sarah Blasko', 'The Overture & The Underscore', 'Perfect Now', 60, 213640),
-        ('Sarah Blasko', 'The Overture & The Underscore', 'Sweet November', -1, 235400),
-        ('Sarah Blasko', 'The Overture & The Underscore', 'Cinders', 50, 249480),
-        ('Sarah Blasko', 'The Overture & The Underscore', 'True Intentions', 40, 251784),
-        ('Sarah Blasko', 'The Overture & The Underscore', 'Remorse', 40, 938504),
-        ('Laura Marling', "A Creature I Don't Know", 'The Muse', 60, 221335),
-        ('Laura Marling', "A Creature I Don't Know", 'I Was Just A Card', 60, 210755),
-        ('Laura Marling', "A Creature I Don't Know", "Don't Ask Me Why", 40, 238968),
-        ('Laura Marling', "A Creature I Don't Know", 'Salinas', 60, 277342),
-        ('Laura Marling', "A Creature I Don't Know", 'The Beast', 60, 345573),
-        ('Laura Marling', "A Creature I Don't Know", 'Night After Night', 60, 308532),
-        ('Laura Marling', "A Creature I Don't Know", 'My Friends', 60, 238367),
-        ('Laura Marling', "A Creature I Don't Know", 'Rest In Bed', 40, 188525),
-        ('Laura Marling', "A Creature I Don't Know", 'Sophia', 80, 291996),
-        ('Laura Marling', "A Creature I Don't Know", 'All My Rage', 60, 171650)
-
-
-    ]
-
-    test_df = pd.DataFrame(test_list,columns=['Artist', 'Album', 'Title', 'Rating', 'Duration'])
-
-    return test_df
+    return df
 
 def ranking_alg(df):
 
@@ -97,25 +42,32 @@ def ranking_alg(df):
     df['timerating'] = df.Duration * df.Rating
 
     #Add a timerating based on sqrt of duration
+    #This is intended to reduce influence of long songs
+    #and avoid albums dominated by one or more very long song getting overrated
+    #e.g. Alice's Restaurant
     df['sqrt_of_duration'] = df['Duration'].pow(0.5)
     df['timerating_2'] = df.sqrt_of_duration * df.Rating
 
     #Make higher rated songs count more
-    # df['rating_powered'] = df.Rating.pow(2)
+    df['rating_squared'] = df.Rating.pow(2) / 100
+    df['timerating_3'] = df.rating_squared * df.sqrt_of_duration
 
     #Group by album
     df.drop(['Title'],axis=1, inplace=True)
-    grouped_df = df.groupby(['Artist','Album'], axis = 0).agg({'timerating': 'sum', 'timerating_2': 'sum', 'Duration': 'sum', 'sqrt_of_duration': 'sum'})
+    grouped_df = df.groupby(['Album', 'Artist'], axis = 0)\
+        .agg({'Rating': 'mean', 'timerating': 'sum', 'timerating_2': 'sum', 'timerating_3': 'sum', 'Duration': 'sum', 'sqrt_of_duration': 'sum'})
 
     #Need to drop any album that doesn't have all its songs rated
 
-    #Make a new df with the minimum rating from each album
+    #Make a new df with the minimum rating and standard deviation of rating for each album
     #Then merge it with the main df
-    elimination_df = df.groupby(['Artist', 'Album'], axis = 0).agg({'Rating': 'min'})
-    elimination_df.rename(columns = {'Rating':'AlbumMinRating'}, inplace =True)
+    helper_df = df.groupby(['Album', 'Artist'], axis = 0).agg(
+                                                            AlbumMinRating = ('Rating', 'min'),
+                                                            AlbumSigma = ('Rating', 'std'),
+                                                            AlbumDuration = ('Duration', 'sum'))\
+                                                            .fillna(0)
 
-    grouped_df = grouped_df.merge(elimination_df, how = 'left', on = ['Artist', 'Album'])
-
+    grouped_df = grouped_df.merge(helper_df, how = 'left', on = ['Album', 'Artist'])
 
     #Get a list of albums that are not fully rated
     unranked_albums_df = grouped_df[grouped_df.AlbumMinRating == -1]
@@ -123,9 +75,18 @@ def ranking_alg(df):
 
     print("Number of unranked albums: {}".format(unranked_albums_df.shape[0]))
 
-
     #Drop all rows with min rating -1 (means unrated in MM)
     grouped_df = grouped_df[grouped_df.AlbumMinRating != -1]
+
+    #Get mean and sigma of album lengths
+
+    AlbumMeanLength = grouped_df.AlbumDuration.mean() / 1000 / 60
+    print("Album Mean Length = {}".format(AlbumMeanLength))
+
+    AlbumLengthSigma = grouped_df.AlbumDuration.std() / 1000 / 60
+    print("Album Length Sigma = {}".format(AlbumLengthSigma))
+
+    print("Album Max Length = {}".format(grouped_df.AlbumDuration.max()))
 
     #Calculate the Piprating (TM). This is an extremely basic first attempt (simple time-weighted average)
     grouped_df['piprating'] = grouped_df.timerating / grouped_df.Duration * 100
@@ -135,25 +96,55 @@ def ranking_alg(df):
     grouped_df['piprating_2'] = grouped_df.timerating_2 / grouped_df.sqrt_of_duration * 100
     grouped_df['piprating_2'] = grouped_df['piprating_2'].astype(int)
 
+    #Calculate version 3 of Piprating(TM)
+    grouped_df['piprating_3'] = grouped_df.timerating_3 / grouped_df.sqrt_of_duration * 100
+    grouped_df['piprating_3'] = grouped_df['piprating_3'].astype(int)
+
+    #Calculate version 4 of Piprating(TM)
+    grouped_df['piprating_4'] = grouped_df.timerating_2 / grouped_df.sqrt_of_duration * 100 * (1 + grouped_df.AlbumSigma / 60).pow(0.2)
+    grouped_df['piprating_4'] = grouped_df['piprating_4'].astype(int)
+
+    #Calculate version 5 of Piprating(TM)
+    bin_labels = [1,2,3,4,5,6,7,8,9,10]
+    grouped_df['rating_bin'] = pd.cut(grouped_df['piprating_4'], bins = 10, labels=bin_labels)
+    grouped_df['rating_bin'] = grouped_df['rating_bin'].astype(int)
+
+    grouped_df['duration_bin'] = pd.cut(grouped_df['AlbumDuration'], bins = 10, labels=bin_labels)
+    grouped_df['duration_bin'] = grouped_df['duration_bin'].astype(int)
+    # print(grouped_df.AlbumDuration.describe(percentiles=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1])/60000)
+    grouped_df['pipscalefactor'] = (-0.03 * grouped_df['rating_bin'] +1 ) * (1-(grouped_df['duration_bin']-1)/9) \
+                                    + (0.06 * grouped_df['rating_bin'] + 0.6) * (grouped_df['duration_bin']-1)/9
+
+    grouped_df['piprating_5'] = grouped_df['piprating_4'] * grouped_df['pipscalefactor']
+
+
     #Drop unnecessary columns
-    grouped_df.drop(['timerating', 'Duration', 'AlbumMinRating'], axis=1,inplace=True)
+    grouped_df.drop(['timerating', 'Duration', 'timerating_2',
+                     'sqrt_of_duration','timerating_3','AlbumMinRating'
+                     ,'AlbumSigma', 'AlbumDuration'], axis=1,inplace=True)
 
     return grouped_df, unranked_albums_df
+
+def ratings_binning(df1):
+
+    df1.loc[df1.Rating < 0, 'Rating'] = 0 #unrated tracks in MM have rating value -1 in the db file
+
+    df1 = df1.groupby(['Rating'], axis=0).agg({'Title': 'count'})
+
+    return df1
 
 
 if __name__ == '__main__':
 
-    output_list = []
+    input_list = []
 
     read_db()
 
-    output_df = create_dataframe(output_list)
+    input_df = create_dataframe(input_list)
 
-    # output_df = create_test_dataframe() #uncomment this line to use the test df
+    ranked_df, unranked_df = ranking_alg(input_df)
 
-    ranked_df, unranked_df = ranking_alg(output_df)
-
-    ranked_df.sort_values(by = "piprating", ascending = False, inplace = True)
+    ranked_df.sort_values(by="piprating_5", ascending = False, inplace = True)
 
     ranked_df.reset_index(inplace=True)
 
@@ -163,6 +154,11 @@ if __name__ == '__main__':
 
     print("Number of ranked albums: {}".format(ranked_df.shape[0]))
 
+    #Put individual song ratings into bins
+    binned_df = create_dataframe(input_list)
+    binned_df = ratings_binning(binned_df)
+
     with pd.ExcelWriter('album_rankings.xlsx') as writer:
         ranked_df.to_excel(writer, sheet_name = "Album Rankings")
         unranked_df.to_excel(writer, sheet_name="Albums to be ranked")
+        binned_df.to_excel(writer, sheet_name="Ratings Histogram")
